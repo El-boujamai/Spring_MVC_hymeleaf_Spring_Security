b822d5d54933619aee83bbc2f1ef62ca5353840a
El-boujamai <ouardiaelboujamaai70@gmail.com>
2026-03-11 01:20:35 +0000

    Seconde  commit

--- src/main/java/com/example/Ouardia/web/ProductController.java
+++ src/main/java/com/example/Ouardia/web/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,8 +19,10 @@ public class ProductController {
         List<Product> products = productRepsitory.findAll();
         model.addAttribute("productList",products);
         return "products";}
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name ="id")Long id ){
+      productRepsitory.deleteById(id);
+        return "redirect:/index";
+    }
 
-//    public ProductController(ProductRepsitory productRepsitory) {
-//        this.productRepsitory = productRepsitory;
-//    }
 }
